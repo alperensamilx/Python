681d3e9d41c032077037f363b372815f09a69aeb
alperensamilx <71771072+alperensamilx@users.noreply.github.com>
2021-03-04 21:41:07 +0300

    Update guess.py

--- guess.py
+++ guess.py
@@ -3,7 +3,7 @@ from random import randint
 name = input('Hello what is your name? ')
 print(f'Well, {name}, I am thinking of a number between 1 and 20.')
 
-number = randint(1, 20)
+secret_number = randint(1, 20)
 lives = 5
 
 
@@ -12,20 +12,20 @@ while lives > 0:
     try:
         guess = int(input('Take a guess. '))
 
-        if guess < number:
+        if guess < secret_number:
             print('Your guess is too low.')
-        elif guess > number:
+        elif guess > secret_number:
             print('Your guess is too high.')
         else:
-            print(f'Good job, {name}! You guessed my number in {number} guesses!')
+            print(f'Good job, {name}! You guessed my number in {secret_number} guesses!')
             break
 
-        if guess != number:
+        if guess != secret_number:
             lives -=1
             print(f'You have {lives} left.')
         
         if lives == 0:
-            print(f'Nope the number I was thinking of was {number}')
+            print(f'Nope the number I was thinking of was {secret_number}')
             
         
 
