1e440e30cb0da535292d98defd4b40b12606fd91
alperensamilx <71771072+alperensamilx@users.noreply.github.com>
2021-03-04 17:24:01 +0300

    Update guess.py

--- guess.py
+++ guess.py
@@ -4,14 +4,13 @@ name = input('Hello what is your name? ')
 print(f'Well, {name}, I am thinking of a number between 1 and 20.')
 
 number = randint(1, 20)
-live = 5
+lives = 5
 
 
-while live > 0:
+while lives > 0:
 
     try:
-        guess = int(input('Take a guess '))
-        live -=1
+        guess = int(input('Take a guess. '))
 
         if guess < number:
             print('Your guess is too low.')
@@ -20,9 +19,15 @@ while live > 0:
         else:
             print(f'Good job, {name}! You guessed my number in {number} guesses!')
             break
+
+        if guess != number:
+            lives -=1
+            print(f'You have {lives} left.')
         
-        if live == 0:
+        if lives == 0:
             print(f'Nope the number I was thinking of was {number}')
+            
+        
 
     except ValueError:
         print('You did not enter a number')
