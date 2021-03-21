299a74fc755c8eaae9a07bd6f7e3c9181fddb726
alperensamilx <71771072+alperensamilx@users.noreply.github.com>
2021-03-21 12:19:52 +0300

    Add files via upload

---
+++ password-manager-start/main.py
@@ -0,0 +1,115 @@
+from tkinter import *
+from tkinter import messagebox
+import random
+import pyperclip
+import json
+
+
+# Password Generator
+
+
+def generate_password():
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't',
+               'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N',
+               'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+    password_letters = [random.choice(letters) for _ in range(random.randint(8, 10))]
+    password_symbols = [random.choice(symbols) for _ in range(random.randint(2, 4))]
+    password_numbers = [random.choice(numbers) for _ in range(random.randint(2, 4))]
+
+    password_list = password_letters + password_symbols + password_numbers
+    random.shuffle(password_list)
+
+    password = "".join(password_list)
+    password_entry.insert(0, password)
+    pyperclip.copy(password)
+
+# Save Password
+
+
+def save():
+    website = website_entry.get()
+    email = email_entry.get()
+    password = password_entry.get()
+    new_data = {website: {
+        "email": email,
+        "password": password,
+    }}
+
+    if len(website) == 0 or len(email) == 0 or len(password) == 0:
+        messagebox.showinfo(title="Oops", message="Please make sure you haven't any fields empty!")
+    else:
+        is_ok = messagebox.askokcancel(title=website, message=f"These are the details entered: \nEmail: {email}"
+                                                              f"\n Password: {password} \nIs it ok to save?")
+        if is_ok:
+            try:
+                with open("data.json", "r") as data_file:
+                    data = json.load(data_file)
+            except FileNotFoundError:
+                with open("data.json", "w") as data_file:
+                    json.dump(new_data, data_file, indent=4)
+            else:
+                data.update(new_data)
+
+                with open("data.json", "w") as data_file:
+                    json.dump(data, data_file, indent=4)
+            finally:
+                website_entry.delete(0, END)
+                password_entry.delete(0, END)
+# Find Password
+
+
+def find_password():
+    website = website_entry.get()
+    with open("data.json") as data_file:
+        data = json.load(data_file)
+        if website in data:
+            email = data[website]["email"]
+            password = data[website]["password"]
+            messagebox.showinfo(title=website, message=f"Email: {email}\nPassword: {password}")
+
+# UI Setup
+window = Tk()
+window.title("Password Manager")
+window.config(padx=50, pady=50)
+
+canvas = Canvas(width=200, height=200)
+logo_img = PhotoImage(file="logo.png")
+canvas.create_image(100, 100, image=logo_img)
+canvas.grid(column=1, row=0)
+
+# Labels
+website_text = Label(text="Website:")
+website_text.grid(column=0, row=1)
+
+email_text = Label(text="Email/Username:")
+email_text.grid(column=0, row=2)
+
+password_text = Label(text="Password:")
+password_text.grid(column=0, row=3)
+
+# Entries
+website_entry = Entry(width=21)
+website_entry.grid(column=1, row=1)
+website_entry.focus()
+
+email_entry = Entry(width=35)
+email_entry.grid(column=1, row=2, columnspan=2)
+email_entry.insert(0, "alperensamil05@gmail.com")
+
+password_entry = Entry(width=21)
+password_entry.grid(column=1, row=3)
+
+# Buttons
+generate_password_button = Button(text="Generate Password", command=generate_password)
+generate_password_button.grid(column=2, row=3)
+
+add_button = Button(text="Add", width=36, command=save)
+add_button.grid(column=1, row=4, columnspan=2)
+
+search_button = Button(text="Search", width=13, command=find_password)
+search_button.grid(row=1, column=2)
+
+window.mainloop()
